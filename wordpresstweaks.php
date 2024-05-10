3d55c97086c1e0854e4e9b8a1290e485e924ed4d
Scot Newbury <scot@scotnewbury.com>
2024-05-10 01:22:22 -0400

    Added minimum PHP version and namespace

--- wordpresstweaks.php
+++ wordpresstweaks.php
@@ -3,9 +3,12 @@
 /**
  * Plugin Name: WordPress Tweaks
  * Description: A group of tweaks for the WordPress admin sections
- * Author: Scot Newbury
- * Version: 0.1
+ * Author:      Scot Newbury
+ * Requires     PHP: 7.4
+ * Version:     0.1
  */
+
+namespace ScotNewbury\WordPressTweaks;
 
 /**
  *  Stop execution of the plugin if it's call directly.
