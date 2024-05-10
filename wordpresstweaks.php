2f308c28a3bd3e48cb93c896f651e6d4f6b07760
Scot Newbury <scot@scotnewbury.com>
2024-05-10 01:24:26 -0400

    Added function to remove 'Howdy,' from the admin bar

--- wordpresstweaks.php
+++ wordpresstweaks.php
@@ -16,3 +16,17 @@ namespace ScotNewbury\WordPressTweaks;
 if (!defined('ABSPATH')) {
   exit;
 }
+
+/**
+ * This function will remove the 'Howdy,' from the admin bar
+ *
+ * @param object $wp_admin_bar
+ * @return void
+ */
+function remove_howdy_admin_bar($wp_admin_bar)
+{
+  $user_account = $wp_admin_bar->get_node('my-account');
+  $remove_title = str_replace('Howdy,', '', $user_account->title);
+  $wp_admin_bar->add_node(array('id' => 'my-account', 'title' => $remove_title));
+}
+add_filter('admin_bar_menu', 'ScotNewbury\WordPressTweaks\remove_howdy_admin_bar', 25);
