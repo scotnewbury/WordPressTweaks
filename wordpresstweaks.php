5104fd8ac276b6419e4e99499add1f32f98ee815
Scot Newbury <scot@scotnewbury.com>
2024-05-10 00:17:54 -0400

    Initial commit - allows plugin to activate

---
+++ wordpresstweaks.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: WordPress Tweaks
+ * Description: A group of tweaks for the WordPress admin sections
+ * Author: Scot Newbury
+ * Version: 0.1
+ */
+
+/**
+ *  Stop execution of the plugin if it's call directly.
+ */
+if (!defined('ABSPATH')) {
+  exit;
+}
